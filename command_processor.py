d7c8982abf53fcfd69c8e7724f7f41011c129953
Ran Bao <worksev@gmail.com>
2014-12-24 12:17:45 +0800

    more comments

--- command_processor.py
+++ command_processor.py
@@ -2,7 +2,7 @@ import re
 import subprocess
 import platform
 
-regex = r"\[command\](?P<command>.+?)\[/command\]"
+regex = r"\[command\](?P<command>.+?)\[/command\]" # (?P<command>.+?) minimum match
 pattern = re.compile(regex)
 
 def cmd_filter(text):
@@ -12,6 +12,7 @@ def cmd_filter(text):
 def cmd_exec(cmds):
     outputs = [("Host", platform.platform())]
     for cmd in cmds:
+        # Append stdout or catch error
         try:
             process = subprocess.Popen(cmd.split(),
                 stdout=subprocess.PIPE,
@@ -27,5 +28,4 @@ def process_command(text):
 
 if __name__ == "__main__":
     text = "[command]pwd[/command]"
-    cmds = cmd_filter(text)
-    print(cmd_exec(cmds))
+    print(process_command(text))
